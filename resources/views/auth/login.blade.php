94ded47b3904131c8bf682a5bee1271f0c938928
cerenuzer <157413970+cerenuzer@users.noreply.github.com>
2025-04-01 15:49:41 +0200

    Giriş formu metinleri Türkçe'ye çevrildi.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -81,39 +81,13 @@ License: For each use you must have a valid license purchased only from above li
 								<!--begin::Heading-->
 								<div class="text-center mb-11">
 									<!--begin::Title-->
-									<h1 class="text-dark fw-bolder mb-3">Sign In</h1>
+									<h1 class="text-dark fw-bolder mb-3">Giriş Yap</h1>
 									<!--end::Title-->
-									<!--begin::Subtitle-->
-									<div class="text-gray-500 fw-semibold fs-6">Your Social Campaigns</div>
-									<!--end::Subtitle=-->
+									
 								</div>
 								<!--begin::Heading-->
-								<!--begin::Login options-->
-								<div class="row g-3 mb-9">
-									<!--begin::Col-->
-									<div class="col-md-6">
-										<!--begin::Google link=-->
-										<a href="#" class="btn btn-flex btn-outline btn-text-gray-700 btn-active-color-primary bg-state-light flex-center text-nowrap w-100">
-										<img alt="Logo" src="assets/media/svg/brand-logos/google-icon.svg" class="h-15px me-3" />Sign in with Google</a>
-										<!--end::Google link=-->
-									</div>
-									<!--end::Col-->
-									<!--begin::Col-->
-									<div class="col-md-6">
-										<!--begin::Google link=-->
-										<a href="#" class="btn btn-flex btn-outline btn-text-gray-700 btn-active-color-primary bg-state-light flex-center text-nowrap w-100">
-										<img alt="Logo" src="assets/media/svg/brand-logos/apple-black.svg" class="theme-light-show h-15px me-3" />
-										<img alt="Logo" src="assets/media/svg/brand-logos/apple-black-dark.svg" class="theme-dark-show h-15px me-3" />Sign in with Apple</a>
-										<!--end::Google link=-->
-									</div>
-									<!--end::Col-->
-								</div>
-								<!--end::Login options-->
-								<!--begin::Separator-->
-								<div class="separator separator-content my-14">
-									<span class="w-125px text-gray-500 fw-semibold fs-7">Or with email</span>
-								</div>
-								<!--end::Separator-->
+								
+								
 								<!--begin::Input group=-->
 								<div class="fv-row mb-8">
 									<!--begin::Email-->
@@ -131,7 +105,7 @@ License: For each use you must have a valid license purchased only from above li
 								<div class="d-flex flex-stack flex-wrap gap-3 fs-base fw-semibold mb-8">
 									<div></div>
 									<!--begin::Link-->
-									<a href="../../demo3/dist/authentication/layouts/overlay/reset-password.html" class="link-primary">Forgot Password ?</a>
+									<a href="../../demo3/dist/authentication/layouts/overlay/reset-password.html" class="link-primary">Şifreni mi unuttun balık hafızalı?</a>
 									<!--end::Link-->
 								</div>
 								<!--end::Wrapper-->
@@ -149,8 +123,8 @@ License: For each use you must have a valid license purchased only from above li
 								</div>
 								<!--end::Submit button-->
 								<!--begin::Sign up-->
-								<div class="text-gray-500 text-center fw-semibold fs-6">Not a Member yet?
-								<a href="../../demo3/dist/authentication/layouts/overlay/sign-up.html" class="link-primary">Sign up</a></div>
+								<div class="text-gray-500 text-center fw-semibold fs-6">henüz üye değil misin?
+								<a href="../../demo3/dist/authentication/layouts/overlay/sign-up.html" class="link-primary">Üye Ol</a></div>
 								<!--end::Sign up-->
 							</form>
 							<!--end::Form-->
